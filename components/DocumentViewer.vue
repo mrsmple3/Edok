7506175a725c4c264afbec44c92b04c41830ce7a
MrSimple <muhammadr0jabxujaev@gmail.com>
2025-02-24 11:07:30 +0500

    google viewer

--- components/DocumentViewer.vue
+++ components/DocumentViewer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="document-viewer">
-    <iframe class="document-frame" src="/uploads/23-02-2025/2.1 Архитектурный_документ_Этап_1-1740334982338-346621630.docx"></iframe>
+    <iframe :src="getGoogleDocViewerUrl(docUrl)" allow="autoplay" class="document-frame"></iframe>
   </div>
 </template>
 
@@ -8,12 +8,17 @@
 const props = defineProps<{
   documentUrl: string
 }>();
+const docUrl = 'https://docs.google.com/document/d/1BGyxBeKzbY0leDOc6QwRAMcWXG6dwPQn/edit';
+// 'http://localhost:3000/uploads/23-02-2025/2.1%20%D0%90%D1%80%D1%85%D0%B8%D1%82%D0%B5%D0%BA%D1%82%D1%83%D1%80%D0%BD%D1%8B%D0%B8%CC%86_%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82_%D0%AD%D1%82%D0%B0%D0%BF_1-1740334982338-346621630.docx'
+function getGoogleDocViewerUrl(url) {
+  return `https://docs.google.com/viewer?url=${url}&embedded=true`
+}
 </script>
 
 <style scoped>
 .document-viewer {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 
 .document-frame {
